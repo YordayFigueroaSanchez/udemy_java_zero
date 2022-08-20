6db52bd1c6ccbcd3fa0da24e18907eec1ad294ac
yfsanchez <yfsanchez@gmail.com>
2022-08-20 17:44:17 -0300

    section 30 : desacoplando el serializador

--- HelloWorld/src/com/yfsanchez/anotaciones/EjemploAnotaciones.java
+++ HelloWorld/src/com/yfsanchez/anotaciones/EjemploAnotaciones.java
@@ -1,10 +1,9 @@
 package com.yfsanchez.anotaciones;
 
 import com.yfsanchez.anotaciones.models.Producto;
+import com.yfsanchez.anotaciones.procesador.JsonSerializador;
 
-import java.lang.reflect.Field;
 import java.time.LocalDate;
-import java.util.Arrays;
 
 public class EjemploAnotaciones {
     public static void main(String[] args) {
@@ -14,29 +13,6 @@ public class EjemploAnotaciones {
         product.setNombre("nombre");
         product.setFecha(LocalDate.now());
 
-        Field[] atributos = product.getClass().getDeclaredFields();
-
-        String json = Arrays.stream(atributos).filter(f -> f.isAnnotationPresent(JsonAtributo.class))
-                .map(f -> {
-                    f.setAccessible(true);
-                    String nombre = f.getAnnotation(JsonAtributo.class).nombre().equals("")
-                            ? f.getName()
-                            : f.getAnnotation(JsonAtributo.class).nombre();
-                    try {
-                        return "\""+nombre + "\":\"" +  f.get(product)+"\"";
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException("ERROR AL SERIALIZAR A JSON " + e.getMessage());
-                    }
-                })
-                .reduce("{", (a,b) -> {
-                    if ("{".equals(a)){
-                        return a+b;
-                    }
-                    return a + "," + b;
-                }).concat("}");
-
-        System.out.println("json = " + json);
-
-
+        System.out.println("json = " + JsonSerializador.toJson(product));
     }
 }
---
+++ HelloWorld/src/com/yfsanchez/anotaciones/procesador/JsonSerializador.java
@@ -0,0 +1,31 @@
+package com.yfsanchez.anotaciones.procesador;
+
+import com.yfsanchez.anotaciones.JsonAtributo;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+public class JsonSerializador {
+    public static String toJson(Object object){
+
+        Field[] atributos = object.getClass().getDeclaredFields();
+        return Arrays.stream(atributos).filter(f -> f.isAnnotationPresent(JsonAtributo.class))
+                .map(f -> {
+                    f.setAccessible(true);
+                    String nombre = f.getAnnotation(JsonAtributo.class).nombre().equals("")
+                            ? f.getName()
+                            : f.getAnnotation(JsonAtributo.class).nombre();
+                    try {
+                        return "\""+nombre + "\":\"" +  f.get(object)+"\"";
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("ERROR AL SERIALIZAR A JSON " + e.getMessage());
+                    }
+                })
+                .reduce("{", (a,b) -> {
+                    if ("{".equals(a)){
+                        return a+b;
+                    }
+                    return a + "," + b;
+                }).concat("}");
+    }
+}
