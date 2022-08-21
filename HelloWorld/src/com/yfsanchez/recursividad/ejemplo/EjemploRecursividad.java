3af05653574a33b4127f62d703450e87fe84e0c2
yfsanchez <yfsanchez@gmail.com>
2022-08-21 17:03:06 -0300

    section 31 : recursividad - mejora visual

--- HelloWorld/src/com/yfsanchez/recursividad/ejemplo/EjemploRecursividad.java
+++ HelloWorld/src/com/yfsanchez/recursividad/ejemplo/EjemploRecursividad.java
@@ -12,26 +12,35 @@ public class EjemploRecursividad {
         Componente disipador = new Componente("disipador");
         Componente tv = new Componente("Nvidia");
         Componente gpu = new Componente("GPU");
-        Componente gpuRam1 = new Componente("PC");
-        Componente gpuRam2 = new Componente("PC");
-        Componente gpuVentiladores = new Componente("PC");
-        Componente ram = new Componente("PC");
-        Componente ssd = new Componente("PC");
+        Componente gpuRam1 = new Componente("GPU ram1");
+        Componente gpuRam2 = new Componente("GPU ram2");
+        Componente gpuVentiladores = new Componente("GPU ventiladores");
+        Componente ram = new Componente("RAM");
+        Componente ssd = new Componente("SSD");
 
         cpu.addComponente(ventilador).addComponente(disipador);
         tv.addComponente(gpu).addComponente(gpuRam1).addComponente(gpuRam2).addComponente(gpuVentiladores);
         placaMadre.addComponente(cpu).addComponente(tv).addComponente(ram).addComponente(ssd);
         pc.addComponente(poder).addComponente(placaMadre).addComponente(new Componente("Teclado")).addComponente(new Componente("Mouse"));
 
-        metodoRecursivo(pc);
+        metodoRecursivo(pc, 0);
     }
 
-    public static void metodoRecursivo(Componente c){
-        System.out.println(c.getNombre());
+    public static void metodoRecursivo(Componente c, int nivel){
+
+        System.out.println(repetirN("\t",nivel) + c.getNombre());
         if (c.tieneHijos()){
             for (Componente hijo: c.getHijos()){
-                metodoRecursivo(hijo);
+                metodoRecursivo(hijo, nivel+1);
             }
         }
+    }
+
+    private static String repetirN(String cadena, int cantidad){
+        String resultado = "";
+        for (int i = 0; i < cantidad; i++) {
+            resultado = resultado.concat(cadena);
+        }
+        return resultado;
     }
 }
