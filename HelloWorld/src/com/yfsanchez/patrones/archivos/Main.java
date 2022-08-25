54e24852ec0b1306419e30825483ee4582788522
yfsanchez <yfsanchez@gmail.com>
2022-08-25 19:36:48 -0300

    section 33 : archivos - creando archivo

---
+++ HelloWorld/src/com/yfsanchez/patrones/archivos/Main.java
@@ -0,0 +1,11 @@
+package com.yfsanchez.patrones.archivos;
+
+import com.yfsanchez.patrones.archivos.servicio.ArchivoServicio;
+
+public class Main {
+    public static void main(String[] args) {
+        String dir = "C:\\utiles\\test.txt";
+        ArchivoServicio servicio = new ArchivoServicio();
+        servicio.crearArchivo(dir);
+    }
+}
---
+++ HelloWorld/src/com/yfsanchez/patrones/archivos/servicio/ArchivoServicio.java
@@ -0,0 +1,16 @@
+package com.yfsanchez.patrones.archivos.servicio;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ArchivoServicio {
+    public void crearArchivo(String nombre){
+        File archivo = new File(nombre);
+        try {
+            FileWriter escritor = new FileWriter(archivo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
